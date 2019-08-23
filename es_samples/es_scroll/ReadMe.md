519022fe1ac429530e66066c5b7d1b0ec91db66d
wararaki <>
2019-08-24 01:08:07 +0900

    update

---
+++ es_samples/es_scroll/ReadMe.md
@@ -0,0 +1,13 @@
+# sample es scroll program
+
+## build
+
+```shell
+docker-compose build
+```
+
+## run
+
+```shell
+docker-compose up
+```
---
+++ es_samples/es_scroll/app/main.py
@@ -0,0 +1,52 @@
+import json
+import os
+import sys
+
+from elasticsearch import Elasticsearch
+
+
+def create_es_query() -> dict:
+    query = {
+        'query': {
+            'match_all': {}
+        },
+        'size': 10
+    }
+    return query
+
+
+def main():
+    es_host = os.getenv('ES_HOST')
+    es_index = 'bank'
+    es_query = create_es_query()
+
+    es = Elasticsearch(hosts=[es_host])
+    response = es.search(
+        index=es_index,
+        body=es_query,
+        request_timeout=1000,
+        scroll='2m'
+    )
+
+    print('scroll start!')
+    scroll_id = response.get('_scroll_id')
+    data = response.get('hits').get('hits')
+    max_size = 100
+    while max_size > len(data):
+        response = es.scroll(
+            scroll_id=scroll_id,
+            scroll='2m'
+        )
+        data.extend(response.get('hits').get('hits'))
+
+        ## show info
+        print(f'size of data: {len(data)}')
+        print(json.dumps(data[-1]))
+
+    print('finish scroll!')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
