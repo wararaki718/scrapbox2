640b5d9f53f0f4bdcc79425698160beffb555d27
wararaki <>
2019-08-24 14:49:39 +0900

    update

---
+++ py_sample/fastapi_test/app/test_main.py
@@ -0,0 +1,12 @@
+from starlette.testclient import TestClient
+
+from main import app
+
+
+client = TestClient(app)
+
+
+def test_hello():
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.json() == {'msg': 'hello'}
