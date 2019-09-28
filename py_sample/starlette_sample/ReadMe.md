172237a8e4d9aee2986555b1c09d14ef77604193
wararaki <>
2019-09-29 00:38:25 +0900

    update

---
+++ py_sample/starlette_sample/ReadMe.md
@@ -0,0 +1,19 @@
+# sample starlette application
+
+## build
+
+```shell
+docker-compose build
+```
+
+## run
+
+```shell
+docker-compose up
+```
+
+check the api
+
+```shell
+curl localhost:8000
+```
---
+++ py_sample/starlette_sample/app/main.py
@@ -0,0 +1,8 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+app = Starlette()
+
+@app.route('/')
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
