988e0b414266b9374b74e2ed98e23d8bccff44f5
wararaki <>
2019-11-21 00:53:52 +0900

    update

--- scala_sample/sample_concat_spark_dataframe/download.sh
+++ scala_sample/sample_concat_spark_dataframe/download.sh
@@ -6,7 +6,5 @@ mkdir -p $DATA_DIR
 ## download data
 wget https://raw.githubusercontent.com/microsoft/LightGBM/master/examples/lambdarank/rank.train -P $DATA_DIR
 wget https://raw.githubusercontent.com/microsoft/LightGBM/master/examples/lambdarank/rank.train.query -P $DATA_DIR
-wget https://raw.githubusercontent.com/microsoft/LightGBM/master/examples/lambdarank/rank.test -P $DATA_DIR
-wget https://raw.githubusercontent.com/microsoft/LightGBM/master/examples/lambdarank/rank.test.query -P $DATA_DIR
 
 echo "download complete"
