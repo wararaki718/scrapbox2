88af0cdbcf4b72e391ba93cc17d1e0b064185702
wararaki <>
2019-11-27 01:18:29 +0900

    update

---
+++ es_samples/es_cerebro_sample/ReadMe.md
@@ -0,0 +1,15 @@
+# sample cerebro
+
+## build & pull images
+
+```shell
+docker-compose build
+```
+
+## run containers
+
+```shell
+docker-compose up
+```
+
+After that, you access on http://localhost:9000 and connect to http://es:9200 on the Cerebro.
---
+++ es_samples/es_cerebro_sample/scripts/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+# set accounts
+
+status='red'
+while [ "$status" != "green" ]
+do
+    echo "elasticsearch status: $status"
+    sleep 10
+    response=`curl ${ES_HOST}:9200/_cat/health?h=status`
+    status=`echo "$response" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g'`
+done
+
+## download
+wget https://download.elastic.co/demos/kibana/gettingstarted/accounts.zip
+unzip accounts.zip
+
+## load data
+curl -H 'Content-Type: application/x-ndjson' -X POST "${ES_HOST}:9200/bank/account/_bulk?pretty" --data-binary @accounts.json
+
+## escape status yellow
+curl -H 'Content-Type: application/json' -X PUT "${ES_HOST}:9200/bank/_settings?pretty" -d '{"number_of_replicas": 0}'
+
+## check status
+curl -XGET "${ES_HOST}:9200/_cat/indices?v&pretty"
+
+## delete data
+rm accounts.json accounts.zip
+
+echo "setup done"
