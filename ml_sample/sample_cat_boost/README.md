982bab5276248c7b54c65a1377838507e652e9a8
wararaki <>
2020-02-01 00:38:35 +0900

    update

---
+++ ml_sample/sample_cat_boost/README.md
@@ -0,0 +1,17 @@
+# sample cat boost
+
+```shell
+poetry install
+```
+
+## download
+
+```shell
+bash download.sh
+```
+
+## run
+
+```shell
+poetry run python sample_cat_boost/main.py
+```
---
+++ ml_sample/sample_cat_boost/sample_cat_boost/main.py
@@ -0,0 +1,33 @@
+from catboost import CatBoostClassifier
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+    
+
+def main():
+    df = pd.read_csv('Pokemon.csv').dropna()
+    y = df['Type 1']
+    X = df.drop(['#', 'Name', 'Type 1',], axis=1)
+    categorical_features_indices = np.where(X.dtypes != np.float)[0]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model = CatBoostClassifier(
+        custom_loss=['Accuracy'],
+        random_seed=42,
+        logging_level='Verbose'
+    )
+    model.fit(
+        X_train,
+        y_train,
+        cat_features=categorical_features_indices,
+        eval_set=(X_test, y_test)
+    )
+    y_preds = model.predict(X_test)
+    print(accuracy_score(y_test, y_preds))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
