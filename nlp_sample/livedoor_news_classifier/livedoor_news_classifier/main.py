5235050b3f257ce623d239b043634d31fd352265
wararaki <ky7.ott.w@gmail.com>
2020-04-04 03:35:18 +0900

    update

--- nlp_sample/livedoor_news_classifier/livedoor_news_classifier/main.py
+++ nlp_sample/livedoor_news_classifier/livedoor_news_classifier/main.py
@@ -1,7 +1,6 @@
 import glob
 import logging
 
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
